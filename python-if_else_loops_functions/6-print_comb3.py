1bfb50c117a6c8586cf77d599378ffad76728460
lenne sebastien <sebastienlenne@gmail.com>
2023-05-30 10:53:37 +0200

    add files task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-for number in range(0, 100):
-    if number < 10:
-        print(f"0{number},", end=" ")
-    else:
-        print(f"{number},", end=" ")
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
+        else:
+            print("{}{}".format(digit1, digit2), end=", ")
