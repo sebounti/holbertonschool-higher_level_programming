193378da5e3c5cd550fc4b7e0bfbc05d5362e966
lenne sebastien <sebastienlenne@gmail.com>
2023-05-31 15:37:21 +0200

    update file task2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -11,4 +11,4 @@ elif count == 1:
 else:
     print("{} arguments:".format(count))
 for i in range (count):
-    print("{}:{}".format(i + 1, sys.argv[i +1]))
+    print("{}: {}".format(i + 1, sys.argv[i + 1]))
