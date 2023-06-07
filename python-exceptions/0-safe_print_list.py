7cc6d6ca7e08292af49eed87cd4d65ee57551146
lenne sebastien <sebastienlenne@gmail.com>
2023-06-07 13:43:32 +0200

    update file  task O

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     try:
         count = 0
-        for index in range (x):
+        for index in range(x):
             print(my_list[index], end="")
             count += 1
     except IndexError:
