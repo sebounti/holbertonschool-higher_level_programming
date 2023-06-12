576958f817d84bc3b2c402aac303c67faef69989
lenne sebastien <sebastienlenne@gmail.com>
2023-06-12 14:25:27 +0200

    add file task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" based on 0-square.py"""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """
+        Initialize a Square.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
