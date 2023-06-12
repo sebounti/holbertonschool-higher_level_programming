9b7649c0fa87361196e073e0d55d80d233218afa
lenne sebastien <sebastienlenne@gmail.com>
2023-06-12 18:27:23 +0200

    update file task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,5 +1,10 @@
+#!/usr/bin/python3
+"""Define a class Square. based on 1-square.py"""
+
+
 class Square:
     """Represent a square."""
+
 
     def __init__(self, size=0, position=(0, 0)):
         """
