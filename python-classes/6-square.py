3b3c5bdcdeeeac914a41570e65d2340c46c9ad1d
lenne sebastien <sebastienlenne@gmail.com>
2023-06-12 18:25:19 +0200

    update file task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,16 +1,15 @@
 class Square:
     """Represent a square."""
 
-
     def __init__(self, size=0, position=(0, 0)):
         """
-        Initialize a new Square.
+        Initialize a new square.
         Args:
             size (int): The size of the new square.
             position (tuple): The position of the new square.
         """
-        self.__size = size
-        self.__position = position
+        self.__size = size  # Initialize the size of the square
+        self.__position = position  # Initialize the position of the square
 
     @property
     def size(self):
@@ -23,8 +22,8 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        else :
-            self.__size = value
+        else:
+            self.__size = value  # Set the size of the square
 
     @property
     def position(self):
@@ -40,21 +39,22 @@ class Square:
             or not all(num >= 0 for num in value)
         ):
             raise TypeError("position must be a tuple of 2 positive integers")
-        else :
-            self.__position = value
+        else:
+            self.__position = value  # Set the position of the square
 
     def area(self):
         """Calculate the area of the square."""
-        return (self.__size) ** 2
+        return self.__size ** 2
 
     def my_print(self):
         """Print the square with the '#' character."""
         if self.__size == 0:
-            print()
+            print()  # Print an empty line if size is 0
             return
         else:
             for x in range(self.__position[1]):
-                print()
+                print()  # Print empty lines before the square
 
             for x in range(self.__size):
                 print(" " * self.__position[0] + "#" * self.__size)
+                # Print the lines of the square
