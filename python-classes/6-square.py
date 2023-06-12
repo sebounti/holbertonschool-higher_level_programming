bb37b94690ec8861b84d458295e7285eb11f8ec7
lenne sebastien <sebastienlenne@gmail.com>
2023-06-12 18:31:20 +0200

    update file task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -5,7 +5,6 @@
 class Square:
     """Represent a square."""
 
-
     def __init__(self, size=0, position=(0, 0)):
         """
         Initialize a new square.
@@ -27,7 +26,6 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        else:
             self.__size = value  # Set the size of the square
 
     @property
@@ -44,7 +42,6 @@ class Square:
             or not all(num >= 0 for num in value)
         ):
             raise TypeError("position must be a tuple of 2 positive integers")
-        else:
             self.__position = value  # Set the position of the square
 
     def area(self):
@@ -55,11 +52,12 @@ class Square:
         """Print the square with the '#' character."""
         if self.__size == 0:
             print()  # Print an empty line if size is 0
-            return
-        else:
-            for x in range(self.__position[1]):
-                print()  # Print empty lines before the square
+            return\
 
-            for x in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
-                # Print the lines of the square
+        for x in range(self.__position[1]):
+            print()
+            # Print empty lines before the square
+
+        for x in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
+            # Print the lines of the square
