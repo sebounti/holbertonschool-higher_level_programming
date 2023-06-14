5cf5cf8f6d9e9fa01b21c4fec76b4f20d918d161
lenne sebastien <sebastienlenne@gmail.com>
2023-06-14 14:43:15 +0200

    update  file task 9  projet 3 mores classes

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -19,6 +19,28 @@ class Rectangle:
         self. width = width
         Rectangle.number_of_instances += 1
 
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        '''
+        returns the biggest rectangle based on the area
+        '''
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        else:
+            return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        '''
+        that returns a new Rectangle instance with width == height == size
+        '''
+        return cls(size, size)
+
     @property
     def height(self):
         '''Get/set the height of the rectangle.'''
@@ -71,25 +93,3 @@ class Rectangle:
     def __del__(self):
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        '''
-        returns the biggest rectangle based on the area
-        '''
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-
-        if rect_1.area() >= rect_2.area():
-            return rect_1
-        else:
-            return rect_2
-
-    @classmethod
-    def square(cls, size=0):
-        '''
-        that returns a new Rectangle instance with width == height == size
-        '''
-        return cls(size, size)
