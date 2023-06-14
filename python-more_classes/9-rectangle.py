ea7483f4d1660e10ef16c5afd8313926740ad392
lenne sebastien <sebastienlenne@gmail.com>
2023-06-14 14:50:29 +0200

    update  file task 9  projet 3 mores classes

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -19,6 +19,10 @@ class Rectangle:
         self. width = width
         Rectangle.number_of_instances += 1
 
+    @classmethod
+    def square(cls, size=0):
+        return cls(size, size)
+
     @property
     def height(self):
         '''Get/set the height of the rectangle.'''
@@ -59,10 +63,6 @@ class Rectangle:
             return rect_1
         else:
             return rect_2
-
-    @classmethod
-    def square(cls, size=0):
-        return cls(size, size)
 
     def area(self):
         """ return area of the rectangle"""
