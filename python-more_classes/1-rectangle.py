01a7730e20074851e6f85a668dc133077fedd032
lenne sebastien <sebastienlenne@gmail.com>
2023-06-14 10:48:52 +0200

    update file task 1 project mores classes python

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -12,21 +12,8 @@ class Rectangle:
         width (int): The width of the new rectangle.
         height (int): The height of the new rectangle.
         '''
-        self. width = width
         self. height = height
-
-    @property
-    def width(self):
-        '''Get/set the width of the rectangle.'''
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError('width must be an integer')
-        if value <= 0:
-            raise ValueError('width must be = 0')
-        self.__width = value
+        self. width = width
 
     @property
     def height(self):
@@ -37,6 +24,19 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
-        if value <= 0:
+        if value < 0:
             raise ValueError('height must be = 0')
         self.__height = value
+
+    @property
+    def width(self):
+        '''Get/set the width of the rectangle.'''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be = 0')
+        self.__width = value
