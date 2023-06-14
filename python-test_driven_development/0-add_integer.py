396c275b3bef5286cc04659fba2a0a9d8971533c
lenne sebastien <sebastienlenne@gmail.com>
2023-06-14 16:35:00 +0200

    update file task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,9 +6,9 @@ def add_integer(a, b=98):
     add two integer
     return Value
     """
-    if not isinstance (a, (int, float)):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance (b, (int, float)):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
     return (int(a) + int(b))
