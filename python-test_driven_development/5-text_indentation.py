dcfa1fce760f8b4b53748f080b893707a3747cc7
lenne sebastien <sebastienlenne@gmail.com>
2023-06-18 23:19:07 +0200

    update file task 5 text_indentation project 2 Driven test

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,17 +6,15 @@ Adds two new lines after a set of characters.
 
 
 def text_indentation(text):
+    """Prints text with added two newlines
+    after each of these characters {'.', '?', ':'}.
     """
-    prints a text with 2 new
-    lines after each of ., ? and :
-    """
-    if not isinstance(text, str):
+
+    if type(text) is not str:
         raise TypeError("text must be a string")
-    text = text.strip()
-    my_text = ""
-    for char in text:
-        if char in [".", "?", ":"]:
-            my_text += char + "\n\n"
-        else:
-            my_text += char
-    print(my_text)
+
+    for delim in ".:?":
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
+
+    print("{}".format(text), end="")
