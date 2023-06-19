7f38e83e683bad7cac3c7a3b7281d265d24ef991
lenne sebastien <sebastienlenne@gmail.com>
2023-06-19 14:29:38 +0200

    add file project python inheritance/ readme end task 0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This script defines a function called lookup that
+returns the list of attributes of an object.
+"""
+def lookup(obj):
+    """
+    This function takes an object as input and
+    returns the list of its attributes.
+
+    Args:
+        obj: The object for which we want to obtain the list of attributes.
+
+    Returns:
+        A list containing the names of the object's attributes.
+    """
+    return dir(obj)
