d871a89483b22c35289c083d631896d2405a3934
lenne sebastien <sebastienlenne@gmail.com>
2023-06-19 21:23:09 +0200

    add file task 6-base geometry /project python inheritance

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a class BaseGeometry based on 5-Base_geometry.py"""
+
+
+class BaseGeometry:
+    """The empty class"""
+
+    def area(self):
+        """ return area """
+        raise Exception("area() is not implemented")
