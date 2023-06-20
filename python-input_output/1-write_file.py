28f13505871199d83b7af83afc636f8657c17761
lenne sebastien <sebastienlenne@gmail.com>
2023-06-20 23:05:32 +0200

    project python input_output/ file readme task 1-write_file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+this module
+
+'''
+
+
+def write_file(filename="", text=""):
+    '''
+    This function writes the specified text to a file.
+
+    Parameters:
+        filename (str): The name of the file to write to.
+        text (str): The text to be written to the file.
+
+    Returns:
+        int: The number of characters written to the file.
+    '''
+    with open(filename, "w" ,encoding="utf-8") as my_file:
+        characters_written = my_file.write(text)
+
+        return characters_written
