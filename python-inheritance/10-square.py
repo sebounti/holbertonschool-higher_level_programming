a86b50b70042ca07ae6ebcb878a9c171440618d7
lenne sebastien <sebastienlenne@gmail.com>
2023-06-20 13:06:34 +0200

    update  file task 10-square/ project inheritance

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -6,9 +6,9 @@ Rectangle = __import__("9-rectangle").Rectangle
 
 class Square (Rectangle):
 
-    def __init__( self, size):
+    def __init__(self, size):
         '''Initializes a Square object.'''
-        self.__size = size
 
-        super().__init__(self.__size, self.__size)
-        super().integer_validator("size", self.__size)
+        super().__init__(size, size)
+        super().integer_validator("size", size)
+        self.__size = size
