833082d4263d1442739246d81ab59cc8cc794a60
lenne sebastien <sebastienlenne@gmail.com>
2023-06-21 10:33:58 +0200

    add file task 8-class to Json/projet input output

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    This module contains a function that returns
+    a default dictionary object
+"""
+
+
+def class_to_json(obj):
+    """
+        Return a dictionary
+    """
+    return (getattr(obj, "__dict__"))
