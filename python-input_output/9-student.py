c4f3b19fe83aa76994eb4f0224f22c6a724c398a
lenne sebastien <sebastienlenne@gmail.com>
2023-06-21 10:59:54 +0200

    update  file task 9-student/projet input output

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -5,9 +5,6 @@ class Student:
     '''A class representing a student.'''
 
     def __init__(self, first_name, last_name, age):
-        '''
-        Initialize a Student object.
-        '''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
