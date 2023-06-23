026912bdf7b46117353c7c3ae42f46230d5ab179
lenne sebastien <sebastienlenne@gmail.com>
2023-06-23 22:40:35 +0200

    update file 7-base geometry to project inheritance

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -22,7 +22,7 @@ class BaseGeometry:
         :raises: TypeError - if value is not an integer
                  ValueError - if value is less than or equal to 0
         """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
+        if not isinstance(value, int):
+              raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
