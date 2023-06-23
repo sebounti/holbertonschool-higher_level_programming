50a2afbc264dd75e7340bb7716c02c24b4312291
lenne sebastien <sebastienlenne@gmail.com>
2023-06-23 22:37:31 +0200

    add file 7-base geometry to project inheritance

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -3,13 +3,25 @@
 
 
 class BaseGeometry:
-    """The empty class"""
+    """An empty class representing the base geometry"""
 
     def area(self):
-        """ return area """
+        """
+        Calculate the area of the geometry
+        :return: None
+        :raises: Exception - area() is not implemented
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """
+        Validate if a value is an integer greater than 0
+        :param name: Name of the value
+        :param value: Value to be validated
+        :return: None
+        :raises: TypeError - if value is not an integer
+                 ValueError - if value is less than or equal to 0
+        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
