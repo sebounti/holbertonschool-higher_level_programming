9ba6636b422bbe36283238df3b429dac9ed9361b
lenne sebastien <sebastienlenne@gmail.com>
2023-06-26 15:16:18 +0200

    update file rectangle.py for project almost a circle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -26,68 +26,32 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        '''
-        Getter for the private attribute __width.
-        '''
         return self.__width
 
     @width.setter
     def width(self, width):
-        '''
-        Setter for the private attribute __width.
-
-        Args:
-            value (int): The new value for the width.
-        '''
         self.__width = width
 
     @property
     def height(self):
-        '''
-        Getter for the private attribute __height.
-        '''
         return self.__height
 
     @height.setter
     def height(self, height):
-        '''
-        Setter for the private attribute __height.
-
-        Args:
-            value (int): The new value for the height.
-        '''
         self.__height = height
 
     @property
     def x(self):
-        '''
-        Getter for the private attribute __x.
-        '''
-        return self.x
+        return self.__x
 
     @x.setter
     def x(self, x):
-        '''
-        Setter for the private attribute __x.
-
-        Args:
-            value (int): The new value for the x.
-        '''
         self.__x = x
 
     @property
     def y(self):
-        '''
-        Getter for the private attribute __y.
-        '''
-        return self.y
+        return self.__y
 
     @y.setter
     def y(self, y):
-        '''
-        Setter for the private attribute __y.
-
-        Args:
-            value (int): The new value for the y.
-        '''
         self.__y = y
