3562b046b3aa966251eeb05150e078ff0382e1cc
lenne sebastien <sebastienlenne@gmail.com>
2023-06-26 14:36:56 +0200

    update file rectangle.py for project almost a circle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -17,11 +17,12 @@ class Rectangle(Base):
             id (int, optional): The identifier to assign to the instance.
     '''
     def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = x
+
+        super().__init__(id)
 
     @property
     def width(self):
