ed68cddd91546e4a1c541439664e13e1135c00da
lenne sebastien <sebastienlenne@gmail.com>
2023-06-26 10:54:40 +0200

    new files probject almost a circle

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This file contains the definition of the Base class.
+"""
+
+
+class Base:
+    __nb_objects = 0
+    '''
+    Constructor of the Base class.
+
+        Args:
+        id (int, optional): The identifier to assign to the instance.
+        If not specified, a new identifier will be generated.
+    '''
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
