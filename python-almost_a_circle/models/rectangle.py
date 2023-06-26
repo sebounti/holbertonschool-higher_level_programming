a2da00b838d33ea8672132fa1c08d11aad8fd9d9
lenne sebastien <sebastienlenne@gmail.com>
2023-06-26 18:31:33 +0200

    update file rectangle.py task 4  for project almost a circle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -78,3 +78,9 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         else:
             self.__y = value
+
+    def area(self):
+        '''
+        area of the rectangle
+        '''
+        return self.width * self.height
