74270d20bedf30470ae9497bfb1d016493df31dd
lenne sebastien <sebastienlenne@gmail.com>
2023-06-27 18:56:23 +0200

    add file base.py task 15 for project almost a circle

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 This file contains the definition of the Base class.
 """
+import json
 
 
 class Base:
@@ -26,3 +27,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """
+        Converts a list of dictionaries to a JSON string.
+        """
+        if list_dictionaries is None:
+            return ("[]")
+        else:
+            return json.dumps(list_dictionaries)
