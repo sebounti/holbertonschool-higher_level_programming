09438c225b677b1fd2ae3562f7a18318d06c748c
lenne sebastien <sebastienlenne@gmail.com>
2023-06-27 14:42:28 +0200

    add file square.py task 11 for project almost a circle

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -22,7 +22,15 @@ class Square (Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
-    moq
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self,value):
+            self.width = value
+            self.height = value
+
 
     def __str__(self):
         '''Returns a string representation of the Square.'''
