947654375aaeb67c0d9b65492b4006058972f250
lenne sebastien <sebastienlenne@gmail.com>
2023-06-28 13:59:27 +0200

    add file base.py task 19 for project almost a circle

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -80,3 +80,20 @@ class Base:
             dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """
+
+        Returns:
+            _type_: _description_
+        """
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, 'r') as file:
+                json_data = file.read()
+                data = cls.from_json_string(json_data)
+                instances = [cls.create(**instance) for instance in data]
+                return instances
+        except FileNotFoundError:
+            return []
