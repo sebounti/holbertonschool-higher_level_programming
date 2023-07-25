124f350a13fb925d73975222eecc95c45956524a
lenne sebastien <sebastienlenne@gmail.com>
2023-07-25 11:10:44 +0200

    update file projet sql more queries task 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- This script gives full privileges to the MySQL localhost server.
-SHOW GRANTS for uuser_0d_1@localhost;
-SHOW GRANTS for uuser_0d_2@localhost;
+SHOW GRANTS for 'user_0d_1'@localhost;
+SHOW GRANTS for 'user_0d_2'@localhost;
