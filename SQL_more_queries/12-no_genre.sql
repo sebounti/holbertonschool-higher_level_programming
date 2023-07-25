8a4fd7110e06c8d33eb8ec13d332129e72e05c78
lenne sebastien <sebastienlenne@gmail.com>
2023-07-26 00:11:06 +0200

    add  file task 12 for project SQL More queries

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- This script lists all shows contained in hbtn_0d_tvshows without a genre linked.
+SELECT TV_SHOWS.title, TV_SHOW_GENRES.genre_id
+FROM TV_SHOWS
+LEFT JOIN TV_SHOW_GENRES
+ON TV_SHOWS.id = TV_SHOW_GENRES.show_id
+WHERE TV_SHOW_GENRES.genre_id IS NULL
+ORDER BY TV_SHOWS.title, TV_SHOW_GENRES.genre_id;
