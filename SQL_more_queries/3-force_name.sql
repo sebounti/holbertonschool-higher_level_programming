1412eed757f5f8797a8c2c8aee902afbf981f02a
lenne sebastien <sebastienlenne@gmail.com>
2023-07-25 15:03:03 +0200

    update file task 3 for projet SQL more queries

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
-/* This script creates a table force_name on your MySQL server */
+-- This script creates a table force_name on your MySQL server.
 CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
