ed1212e1cd0740146320e05b92bd28172f487848
lenne sebastien <sebastienlenne@gmail.com>
2023-07-31 11:51:34 +0200

    update  file task O from project python object relational mapping

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,23 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = mysqldb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+    db = MySQLdb.connect(
+        host="localhost", port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
+
+# Créer un objet curseur pour exécuter des requêtes SQL
+    cur = db.cursor()
+
+# Exécuter la requête SQL pour sélectionner toutes les lignes de la table "states"
+    cur.execute("SELECT * FROM states ORDER BY id ASC;")
+# Récupérer toutes les lignes résultantes de la requête dans une liste
+    states = cur.fetchall()
+
+# Parcourir la liste des états récupérée et afficher chaque état
+    for state in states:
+        print(state)
+
+    cur.close()
+    db.close()
