d79036d119556e0ad249ee30dc742a4957bd6bcc
lenne sebastien <sebastienlenne@gmail.com>
2023-07-31 17:41:50 +0200

    update file task 2 project python obj rela mapping

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     cur.execute(
-        "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC", (argv[4],))
+        "SELECT * FROM states WHERE BINARY `name` = '{}'".format(argv[4]))
 
     states = cur.fetchall()
 
