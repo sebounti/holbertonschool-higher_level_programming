af40b13c9764f283795b868f1af951c3e6168fd2
lenne sebastien <sebastienlenne@gmail.com>
2023-07-31 17:07:24 +0200

    update file task 2 project python obj rela mapping

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-Lists all states from the database hbtn_0e_0_usa.
+Script that Lists all states from the database hbtn_0e_0_usa.
 '''
 import MySQLdb
 from sys import argv
@@ -9,9 +9,10 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(
             host="localhost", port=3306, user=argv[1],
-            password=argv[2], db=argv[3])
+            password=argv[2], database=argv[3])
 
     cur = db.cursor()
+
     cur.execute(
         "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC", (argv[4],))
 
