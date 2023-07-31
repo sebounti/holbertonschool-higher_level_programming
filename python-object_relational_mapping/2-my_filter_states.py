b83f36f976a7c4a3a2038b698dcccfb03a637db5
lenne sebastien <sebastienlenne@gmail.com>
2023-07-31 13:47:51 +0200

    add  file task 2 from project python object relational mapping

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+''' Lists all states from the database hbtn_0e_0_usa.'''
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    # Se connecter à la base de données MySQL en utilisant les arguments de ligne de commande
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    # Créer un objet curseur pour exécuter des requêtes SQL
+    cur = db.cursor()
+
+    # Exécuter la requête SQL pour sélectionner les lignes de la table "states" avec le nom spécifié
+    state_name_searched = sys.argv[4]
+    query = "SELECT * FROM `states` WHERE `name` = %s ORDER BY id ASC;"
+
+    cur.execute(query, (state_name_searched,))
+
+    # Récupérer toutes les lignes résultantes de la requête dans une liste et les afficher
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+
+    # Fermer le curseur et la connexion à la base de données
+    cur.close()
+    db.close()
