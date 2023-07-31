0828ef9abff620350414aa3dd7b88312cbbff330
lenne sebastien <sebastienlenne@gmail.com>
2023-07-31 14:00:26 +0200

    update  file task 2 from project python object relational mapping

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 ''' Lists all states from the database hbtn_0e_0_usa.'''
-
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
 
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost", port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
 
     # Créer un objet curseur pour exécuter des requêtes SQL.
     cur = db.cursor()
