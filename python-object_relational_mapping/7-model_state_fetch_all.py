43880e4beb2b14fa499479a2f6cde8b70f766619
lenne sebastien <sebastienlenne@gmail.com>
2023-08-01 10:16:42 +0200

    add file task 7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""lists all State objects from the database hbtn_0e_6_usa"""
+"""
+lists all State objects from the database hbtn_0e_6_usa
+"""
 
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
 
 if __name__ == "__main__":
+
     # Créer le moteur de base de données
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
