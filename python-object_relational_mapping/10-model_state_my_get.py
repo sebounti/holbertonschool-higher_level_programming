085f9a42e0d0b87d6897c6c8fe40419c9bbad9d3
lenne sebastien <sebastienlenne@gmail.com>
2023-08-01 13:43:44 +0200

    add file task 10 for projet object relational mapping

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -9,15 +9,14 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+
 if __name__ == "__main__":
 
-    # make engine for database
     user = argv[1]
     passwd = argv[2]
     db = argv[3]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(user, passwd, db), pool_pre_ping=True)
-
 
     # Initialize engine
     Base.metadata.create_all(engine)
