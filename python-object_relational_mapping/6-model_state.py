e94c37014fb14afad5423bc564e00280f01b7ce2
lenne sebastien <sebastienlenne@gmail.com>
2023-08-01 08:34:39 +0200

    add file task 6 FOR project object relational mapping

--- python-object_relational_mapping/6-model_state.py
+++ python-object_relational_mapping/6-model_state.py
@@ -3,8 +3,8 @@
 """
 import sys
 from model_state import Base, State
-
 from sqlalchemy import (create_engine)
+
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-"""Start link class to table in database
 """
-import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+Definition of a State and an instance Base
+"""
+from SQLAlchemy import Column, Integer, String
+from SQLAlchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+class State(Base):
+    """Inherits from Base
+
+        Args: Base (class): inherits from Base
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False,
+                unique=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
