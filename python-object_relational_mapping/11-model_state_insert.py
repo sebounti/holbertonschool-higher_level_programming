14f328c3a766a2b38e8b706737fab7fa510d46b2
lenne sebastien <sebastienlenne@gmail.com>
2023-08-03 11:07:41 +0200

    update task 11 for projet object relational mapping

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -13,10 +13,8 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-
     # Initialize engine
     Base.metadata.create_all(engine)
-
     # Initialize session
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -25,11 +23,9 @@ if __name__ == "__main__":
     new_state = State(name="Louisiana")
     session.add(new_state)
     session.commit()
-
     # Query
     query = session.query(State).\
         order_by(State.id.desc()).first()
-
     # Print query
     print("{}".format(query.id))
 
